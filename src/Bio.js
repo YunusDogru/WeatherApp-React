f6c263480d2294710076b2c18a92eed10336ec59
Yunus Doğru <96145653+YunusDogru@users.noreply.github.com>
2023-03-02 20:36:33 +0300

    Update Bio.js

--- src/Bio.js
+++ src/Bio.js
@@ -4,7 +4,7 @@ function Bio() {
   return (
     <div className="bio">
         <a href="https://github.com/YunusDogru" target="_blank" className="githublink">
-        <img src="./img/bio.jpg" alt="" className="bioimage"/>
+        <img src="https://raw.githubusercontent.com/YunusDogru/WeatherApp-React/main/public/img/bio.jpg" alt="" className="bioimage"/>
         <h3 className="name">Yunus Doğru</h3>
         <h6 className="job">Front-End Developer</h6>
       </a>
